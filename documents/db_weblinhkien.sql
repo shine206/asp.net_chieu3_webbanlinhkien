7d7c2a06f6f0331052330abedfd70d6c725cbe92
takamakoto <acquybongdem180194@gmail.com>
2017-11-21 14:02:30 +0700

    create root folder

---
+++ documents/db_weblinhkien.sql
@@ -0,0 +1,117 @@
+﻿CREATE DATABASE db_weblinhkien;
+GO
+USE db_weblinhkien;
+GO
+
+CREATE TABLE Products(
+	id_product INTEGER PRIMARY KEY IDENTITY NOT NULL,
+	name NVARCHAR(100),
+	price FLOAT,
+	status INT,
+	promotion VARCHAR(255),
+	tag VARCHAR(255),
+	details TEXT,
+	description TEXT,
+	content TEXT,
+	created_at DATETIME NULL DEFAULT NULL,
+	updated_at DATETIME NULL DEFAULT NULL
+)
+
+CREATE TABLE Images(
+	id_image INT PRIMARY KEY IDENTITY NOT NULL,
+	id_product INT,
+	link_image TEXT,
+	created_at DATETIME NULL DEFAULT NULL,
+	updated_at DATETIME NULL DEFAULT NULL
+)
+
+CREATE TABLE banners (
+  id_banner int PRIMARY KEY IDENTITY NOT NULL,
+  name nvarchar(255) NOT NULL,
+  url text NOT NULL,
+  url_banner text NOT NULL,
+  created_at DATETIME NULL DEFAULT NULL,
+  updated_at DATETIME NULL DEFAULT NULL
+)
+
+CREATE TABLE GroupCategories(
+	id_group_category INT PRIMARY KEY IDENTITY NOT NULL,
+	name NVARCHAR(50),
+	id_category INT,
+	created_at DATETIME NULL DEFAULT NULL,
+	updated_at DATETIME NULL DEFAULT NULL
+)
+
+CREATE TABLE Category(
+	id_category INT PRIMARY KEY IDENTITY NOT NULL,
+	name NVARCHAR(50),
+	created_at DATETIME NULL DEFAULT NULL,
+	updated_at DATETIME NULL DEFAULT NULL
+)
+
+CREATE TABLE Admin_Users(
+	id_admin_user INT PRIMARY KEY IDENTITY NOT NULL,
+	username VARCHAR(20) NOT NULL,
+	password varchar(30) NOT NULL,
+	fullname NVARCHAR(50),
+	email VARCHAR(255) UNIQUE,
+	address TEXT,
+	phone_number INT,
+	role varchar(255) NOT NULL,
+	created_at DATETIME NULL DEFAULT NULL,
+	updated_at DATETIME NULL DEFAULT NULL
+	
+)
+
+CREATE TABLE Users (
+  id_user INT PRIMARY KEY IDENTITY NOT NULL,
+  username NVARCHAR(20) NOT NULL,
+  password varchar(30) NOT NULL,
+  fullname NVARCHAR(50),
+  email varchar(255) UNIQUE NOT NULL,
+  address TEXT,
+  phone INT,
+  created_at DATETIME NULL DEFAULT NULL,
+  updated_at DATETIME NULL DEFAULT NULL
+)
+
+
+
+CREATE TABLE Orders(
+	id_order INT NOT NULL,
+	id_order_detail INT NOT NULL,
+	id_user INT,
+	status INT,
+	payment_method INT,
+	coupon VARCHAR(6),
+	created_at DATETIME NULL DEFAULT NULL,
+	updated_at DATETIME NULL DEFAULT NULL,
+	PRIMARY KEY (id_order, id_order_detail)
+)
+
+CREATE TABLE OrderDetail(
+	id_order_detail INT PRIMARY KEY IDENTITY NOT NULL,
+	id_product INT NOT NULL,
+	quantity INT,
+	current_price FLOAT,
+	created_at DATETIME NULL DEFAULT NULL,
+	updated_at DATETIME NULL DEFAULT NULL
+)
+--drop database db_weblinhkien;
+------------------------------------------------
+--Ràng buộc khóa ngoại
+
+ALTER TABLE Orders
+	add constraint fk_orders_user_id FOREIGN KEY (id_user) REFERENCES Users(id_user)
+
+ALTER TABLE Orders
+	add constraint fk_order_detail_id FOREIGN KEY (id_order_detail) REFERENCES OrderDetail(id_order_detail)
+
+ALTER TABLE OrderDetail
+	add constraint fk_product_id FOREIGN KEY (id_product) REFERENCES Products(id_product)
+	
+ALTER TABLE GroupCategories
+	add constraint fk_category_id FOREIGN KEY (id_category) REFERENCES Category(id_category)
+	
+ALTER TABLE Images
+	add constraint fk_image_id FOREIGN KEY (id_product) REFERENCES Products(id_product)
