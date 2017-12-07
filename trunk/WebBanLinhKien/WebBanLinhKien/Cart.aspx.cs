ad674c3b56acb71bf30d88f2c68446f876595a3f
TuLC2097 <tulc20111997@gmail.com>
2017-12-07 15:43:41 +0700

    Update Cart ver 1.0 07/12/2017

--- trunk/WebBanLinhKien/WebBanLinhKien/Cart.aspx.cs
+++ trunk/WebBanLinhKien/WebBanLinhKien/Cart.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
 
 namespace WebBanLinhKien
 {
@@ -11,7 +12,28 @@ namespace WebBanLinhKien
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+           
         }
+
+        protected void btnDel_Click(object sender, EventArgs e)
+        {
+            
+            Response.Redirect("~/Home.aspx");
+        }
+
+        
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Home.aspx");
+        }
+
+        protected void btnContinue_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Home.aspx");
+        }
+
+
+
+        
     }
 }
